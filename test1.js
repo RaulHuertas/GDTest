1f3fa2f5ef791ae4cb86b2c27e428bb7d41d1c0a
rhuertas <raul.huertas@gdpteam.com>
2022-08-25 17:22:20 -0500

    only fails test 2

--- test1.js
+++ test1.js
@@ -3,20 +3,10 @@
 function minCost(N, H, M) {
   console.log("H: " + H);
   console.log("M: " + M);
-  var result = -404
-  maxNumber = Math.max(...H)
-  //Without taking cost into consideration, as a first aproximation, a safe way 
-  //to have nonrepetitive heights is assign maxNumber+1,maxNumber+2, etc...
-  //to the initially repetitive heights
-  newPosibleValue = maxNumber + 1
-  firstIncrements = Array(N).fill(0)
-  for (let i = 1; i < N; i++) {
-    if (H[i] == H[i - 1]) {
-      firstIncrements[i] = newPosibleValue - H[i]
-      newPosibleValue++;
-    }
-  }
-  //We have a solution. Now we need to optimize it
+
+
+
+  //UTILITIES
   totalCost = function (posibleIncrements) {//handy function to calculate cost
     var cost = 0;
     for (let i = 0; i < posibleIncrements.length; i++) {
@@ -27,80 +17,80 @@ function minCost(N, H, M) {
 
   areHeightsOkQ = function (posibleIncrements) {//handy function to check if teh solution is Ok
     var ret = true;
-    console.log(`areHeightsOkQ, START, ${posibleIncrements}`)
+    //console.log(`areHeightsOkQ, START, ${posibleIncrements}`)
     for (let i = 1; i < posibleIncrements.length; i++) {
       let prevTower = H[i - 1] + posibleIncrements[i - 1]
       let thisTower = H[i] + posibleIncrements[i]
-      console.log(`areHeightsOkQ, prevTower: ${prevTower}`)
-      console.log(`areHeightsOkQ, thisTower: ${thisTower}`)
+      //console.log(`areHeightsOkQ, prevTower: ${prevTower}`)
+      //console.log(`areHeightsOkQ, thisTower: ${thisTower}`)
       if (thisTower == prevTower) {
-        console.log('areHeightsOkQ, reject!')
+        //console.log('areHeightsOkQ, reject!')
         ret = false
         break;
       }
     }
-    console.log('areHeightsOkQ, END')
+    //console.log('areHeightsOkQ, END')
     return ret
   }
-  var initialCost = totalCost(firstIncrements)
-  var currentCost = initialCost
-  maxIncrement = Math.max(...firstIncrements)
-  newCost = -1
-  console.log("max: " + maxNumber);
-  console.log("maxIncrement: " + maxIncrement);
-  console.log("naive cost: " + currentCost);
-  var bestIncrements = firstIncrements.slice()
-  while (newCost != currentCost) {
-    //var posibleIncrements = bestIncrements.slice()
-    for (let i = (N-1); i >= 0; i--) {//because of our first aproximation, the last increments are initially the highest, 
-        if((bestIncrements[i-1] == 0)&&(bestIncrements[i] == 0)) {
-            //no need to change this values
-            continue
-        }
-        var newProposition
-        //at least two posibilities, decrease left or right 
-        var posibility1 = bestIncrements.slice()
-        if(posibility1[i-1]>0){
-          posibility1[i-1]--
-        }
-        p1okQ = areHeightsOkQ(posibility1)
-        var posibility2 = bestIncrements.slice()
-        if(posibility2[i]>0){
-          posibility2[i]--
-        }
-        p2okQ = areHeightsOkQ(posibility2)
-        if(p1okQ&&p2okQ){
-          costleft =  totalCost(posibility1);
-          costRight =  totalCost(posibility2);
-          if(costleft<costRight){
-            newProposition = posibility1
-          }else{
-            newProposition = posibility2
-          }
-        }else if(p1okQ){
-          newProposition = posibility1
-        }else if(p2okQ){
-          newProposition = posibility2
-        }else{
-          newCost = currentCost
-          continue
-        }
-        proposedCost = totalCost(newProposition)
-        if(proposedCost<currentCost){
-          currentCost = proposedCost
-          newCost = proposedCost
-          bestIncrements = newProposition
-        }else{
-          newCost = currentCost
-        }
-    }
-    //newCost = totalCost(bestIncrements)
+  if(areHeightsOkQ(Array(N).fill(0))){
+    //cover a first initial case
+    return 0
   }
-  console.log(`firstIncrements: ${firstIncrements}`)
-  console.log(`bestIncrements: ${bestIncrements}`)
-  console.log(`okQ: ${totalCost(bestIncrements)}`)
 
-  return totalCost(bestIncrements)
+
+
+
+
+
+
+  var result = -404
+  maxNumber = Math.max(...H)
+  minNumber = Math.min(...H)
+  //Without taking cost into consideration, as a first aproximation, a safe way 
+  //to have nonrepetitive heights is assign maxNumber+1,maxNumber+2, etc...
+  //to the initially repetitive heights
+  newPosibleValue = maxNumber + 1
+  var firstIncrements = Array(N).fill(0)
+  for (let i = 1; i < N; i++) {
+    if (H[i] == H[i - 1]) {
+      firstIncrements[i] = newPosibleValue - H[i]
+      newPosibleValue++;
+    }
+  }
+  //We have a solution. Now we need to optimize it
+  
+
+  //Brute force aproach
+  var firstCost = totalCost(firstIncrements)
+  var bestCost = firstCost
+  var bestOption = Array(N).fill(0)
+  var proposition = Array(N).fill(0)
+
+  for (let i = 0; i < N; i++) {
+    minIncrement = 0
+    maxIncrement = maxNumber + N
+    for (let incr = minIncrement; incr < maxIncrement; incr++) {
+      for (let p = 1; p < N; p++) {
+        proposition[p] = bestOption[p]
+      }
+      proposition[i] = incr
+      if (!areHeightsOkQ(proposition)) {
+        continue;
+      }
+      let proposedCost = totalCost(proposition);
+      if (proposedCost < bestCost) {
+        bestCost = proposedCost
+        bestOption = proposition
+      }
+    }
+
+  }
+
+  console.log(`firstIncrements: ${firstIncrements}`)
+  console.log(`bestOption: ${bestOption}`)
+  console.log(`areHeightsOkQ: ${areHeightsOkQ(bestOption)}`)
+
+  return bestCost
 }
 
 //User input
@@ -131,12 +121,19 @@ function stdinLineByLine() {
 
   return stdin;
 }
+
+function finalize() {
+  finalV = minCost(N, H, M)
+  process.exitCode = finalV
+  console.log(finalV)
+  process.exit(finalV)
+}
+
 const stdin = stdinLineByLine();
 processLine = (line) => {
   if (line.length == 0) {
     //end 
-    console.log(minCost(N, H, M))
-    process.exit()
+    finalize()
   } else {
     if (waitingN) {
       temp = line.split(/\n|\s/)
@@ -151,8 +148,7 @@ processLine = (line) => {
       M.push(parseInt(temp[1]))
       readed++
       if (readed == N) {
-        console.log(minCost(N, H, M))
-        process.exit()
+        finalize()
       }
     }
   }
